7afda2d10debe6311b8eec1174119947b6c01493
BL3ND3R1N0 <115467116+BL3ND3R1N0@users.noreply.github.com>
2023-04-29 19:25:56 +0300

    Обновление системы управления
    Добавлены функции для использования нашим автопилотом goStraight, goLeft, goRigth, goBack.
    Добавлена функция имитации клавиатуры с возможностью "зажатия" клавиши kbdSim.

--- test_keyboard_and_mouse_control/test_keyboard_and_mouse_control/Main.cpp
+++ test_keyboard_and_mouse_control/test_keyboard_and_mouse_control/Main.cpp
@@ -4,6 +4,70 @@
 
 using namespace std;
 
+void mouseSim(HWND windowHandle)
+{
+	//PODLIVA
+
+			// Get the window client area.
+	RECT rc;
+	GetClientRect(windowHandle, &rc);
+
+	// Convert the client area to screen coordinates.
+	POINT pt = { rc.left, rc.top };
+	POINT pt2 = { rc.right, rc.bottom };
+	ClientToScreen(windowHandle, &pt);
+	ClientToScreen(windowHandle, &pt2);
+	SetRect(&rc, pt.x, pt.y, pt2.x, pt2.y);
+
+	// Confine the cursor.
+	ClipCursor(&rc);
+
+	SetCursorPos(0, 0);
+
+	SetCapture(windowHandle);
+	mouse_event(MOUSEEVENTF_ABSOLUTE, 100, 0, 0, 0);
+	SetCursorPos(0, 0);
+	ReleaseCapture();
+}
+
+typedef short ABOBA;
+
+void kbdSim(ABOBA key, DWORD time = 100)
+{
+	UINT mappedkey;
+	INPUT input = { 0 };
+	key = VkKeyScan(key);
+	mappedkey = MapVirtualKey(LOBYTE(key), 0);
+	input.type = INPUT_KEYBOARD;
+	input.ki.dwFlags = KEYEVENTF_SCANCODE;
+	input.ki.wScan = mappedkey;
+	SendInput(1, &input, sizeof(INPUT));
+	Sleep(time);
+	input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+	SendInput(1, &input, sizeof(INPUT));
+}
+
+void goStraight()
+{
+	kbdSim('W');
+	return;
+}
+void goLeft()
+{
+	kbdSim('A');
+	return;
+}
+void goRight()
+{
+	kbdSim('D');
+	return;
+}
+void goBack()
+{
+	kbdSim('S');
+	return;
+}
+
 void main()
 {
 	HWND windowHandle = FindWindowA(NULL, "Minecraft* 1.19 - Одиночная игра"); //Minecraft* 1.19 - Одиночная игра
@@ -15,54 +79,12 @@ void main()
 		else
 		{
 			SetForegroundWindow(windowHandle);
-			Sleep(1000);
+			Sleep(2000);
 
-			key = new INPUT;
-			key->type = INPUT_KEYBOARD;
-			key->ki.wVk = 27;
-			key->ki.dwFlags = 0;
-			key->ki.time = 0;
-			key->ki.wScan = 0;
-			key->ki.dwExtraInfo = 0;
-
-			//SendInput(1, key, sizeof(INPUT));
-
-			//key->ki.dwExtraInfo = KEYEVENTF_KEYUP;
-			//SendInput(1, key, sizeof(INPUT));
-			//вроде как должно отжимать кнопку, но она сама отжимается 
-
-			Sleep(1000);
-			key->ki.wVk = 'W';
-			SendInput(1, key, sizeof(INPUT));
-
-			Sleep(1000);
-			key->ki.dwExtraInfo = KEYEVENTF_KEYUP;
-			SendInput(1, key, sizeof(INPUT));
-
-			//PODLIVA
-
-			// Get the window client area.
-			RECT rc;
-			GetClientRect(windowHandle, &rc);
-
-			// Convert the client area to screen coordinates.
-			POINT pt = { rc.left, rc.top };
-			POINT pt2 = { rc.right, rc.bottom };
-			ClientToScreen(windowHandle, &pt);
-			ClientToScreen(windowHandle, &pt2);
-			SetRect(&rc, pt.x, pt.y, pt2.x, pt2.y);
+			kbdSim(27);
+			kbdSim('W', 1000); //when u use this prog u need to switch kbd language no English
 			
-			// Confine the cursor.
-			ClipCursor(&rc);
-
-			//SetCursorPos(0, 0);
-
-			//SetCapture(windowHandle);
-			//mouse_event(MOUSEEVENTF_ABSOLUTE, 100, 0, 0, 0);
-			//SetCursorPos(0, 0);
-			//ReleaseCapture();
-
-			delete key;
+			break;
 		}
 	}
 
