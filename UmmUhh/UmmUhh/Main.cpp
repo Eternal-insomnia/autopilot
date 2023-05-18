500ddb6eee4deac58d22efd9ccd30ab38b3c6376
Titarenko <Titarenkoio233@gmail.com>
2023-05-18 19:38:49 +0300

    UPD: жопа

--- UmmUhh/UmmUhh/Main.cpp
+++ UmmUhh/UmmUhh/Main.cpp
@@ -1,10 +1,100 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include <vector>
 #include <Windows.h>
 #include <iostream>
 
 using namespace std;
 using namespace cv;
+
+enum FLIGHT_MODES
+{
+    TAKEOFF,
+    LANDING,
+    FLY
+};
+
+enum DIRECTION_INSTRUCTIONS
+{
+    LEFT,
+    RIGHT,
+    FORWARD,
+    BACKWARD,
+    LAND
+};
+
+void mouseSim(HWND windowHandle)
+{
+    //PODLIVA
+
+            // Get the window client area.
+    RECT rc;
+    GetClientRect(windowHandle, &rc);
+
+    // Convert the client area to screen coordinates.
+    POINT pt = { rc.left, rc.top };
+    POINT pt2 = { rc.right, rc.bottom };
+    ClientToScreen(windowHandle, &pt);
+    ClientToScreen(windowHandle, &pt2);
+    SetRect(&rc, pt.x, pt.y, pt2.x, pt2.y);
+
+    // Confine the cursor.
+    ClipCursor(&rc);
+
+    SetCursorPos(0, 0);
+
+    SetCapture(windowHandle);
+    mouse_event(MOUSEEVENTF_ABSOLUTE, 100, 0, 0, 0);
+    SetCursorPos(0, 0);
+    ReleaseCapture();
+}
+
+typedef short ABOBA;
+
+void kbdSim(ABOBA key, DWORD time = 100)
+{
+    UINT mappedkey;
+    INPUT input = { 0 };
+    key = VkKeyScan(key);
+    mappedkey = MapVirtualKey(LOBYTE(key), 0);
+    input.type = INPUT_KEYBOARD;
+    input.ki.dwFlags = KEYEVENTF_SCANCODE;
+    input.ki.wScan = mappedkey;
+    SendInput(1, &input, sizeof(INPUT));
+    Sleep(time);
+    input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void goUp()
+{
+    kbdSim('W');
+}
+void goDown()
+{
+    kbdSim('S');
+}
+
+void pitchRight()
+{
+    //kbdSim('E');
+    kbdSim(102);
+}
+
+void pitchLeft()
+{
+    //kbdSim('Q');
+    kbdSim(100); // num left
+}
+
+void bendForward()
+{
+    kbdSim(104); // numpad key up
+}
+void bendBackward()
+{
+    kbdSim(98); // numpad key down
+}
 
 Mat hwnd2mat(HWND hwnd)
 {
@@ -26,13 +116,13 @@ Mat hwnd2mat(HWND hwnd)
     RECT desksize;
     GetClientRect(desktop, &desksize);  //change this to whatever size you want to resize to
 
-    srcheight = windowsize.bottom; //u can use desksize.bottom and rigth but eto ne obyazatel'no
-    srcwidth = windowsize.right;
+    srcheight = desksize.bottom;
+    srcwidth = desksize.right;
 
-    height = windowsize.bottom; // <-- масштабирует размер захватываемого окна под созданное окно(’”…Ќя)
-    width = windowsize.right;
-    //height = desksize.bottom;
-    //width = desksize.right;
+    //height = windowsize.bottom;
+    //width = windowsize.right;
+    height = desksize.bottom;
+    width = desksize.right;
 
     src.create(height, width, CV_8UC4);
 
@@ -77,17 +167,93 @@ auto process_img(Mat& image)
 
     Mat processed_img_stage2;
 
-    cvtColor(processed_img, processed_img_stage2, COLOR_BGR2HSV);
+    cvtColor(processed_img, processed_img_stage2, cv::COLOR_BGR2GRAY);
 
     Mat processed_img_stage3;
 
-    inRange(processed_img_stage2, Scalar(123, 9, 200), Scalar(255, 200, 255), processed_img_stage3); //по HSV он будет 181, 208, 150
-
-    //Mat processed_img_fin;
-
-    //addWeighted(processed_img_stage4, 1.0, processed_img_stage5, 1.0, 0.0, processed_img_fin); //дл€ соединени€ нескольких фильтров
+    Canny(processed_img_stage2, processed_img_stage3, 200, 300);
 
     return processed_img_stage3;
+}
+
+auto press_key(int num)
+{
+    return;
+}
+
+std::vector<DIRECTION_INSTRUCTIONS> findDirection(Mat& img)
+{
+    std::vector<Point> pixels;
+    cv::findNonZero(img, pixels);
+
+    std::vector<DIRECTION_INSTRUCTIONS> instructions;
+
+    Point center(img.size().height, img.size().width);
+
+    Point point = pixels.at(0);
+    
+    if (point.x - center.x > 10)
+        instructions.push_back(RIGHT);
+    if (point.x - center.x < 10)
+        instructions.push_back(LEFT);
+    if (point.y - center.y < 10)
+        instructions.push_back(FORWARD);
+    if (point.y - center.y > 10)
+        instructions.push_back(BACKWARD);
+
+    if (instructions.size() == 0)
+        instructions.push_back(LAND);
+
+    return instructions;    
+}
+
+void controlSystem(DIRECTION_INSTRUCTIONS direction, FLIGHT_MODES mode)
+{
+    switch (mode)
+    {
+    case TAKEOFF:
+        goUp();
+        break;
+    case LANDING:
+        // check for obstacles down (?)
+        goDown();
+        break;
+    default:
+
+        switch (direction)
+        {
+        case RIGHT:
+            pitchRight();
+            break;
+        case LEFT:
+            pitchLeft();
+            break;
+        case FORWARD:
+            bendForward();
+            break;
+        case BACKWARD:
+            bendBackward();
+            break;
+        }
+
+        //if (height < something)
+        //goUp();
+        //if (height > something)
+            //goDown();
+
+
+
+        //if (1) // if mark is to the right from center on minimap
+        //    pitchRight();
+        //else if (1) // if mark is to the left
+        //    pitchLeft();
+        ////else
+        //bendForward();
+        //if (1) // if need to adjust height
+        //    goUp();
+        //else if (1) // if helicopter is too high
+        //    goDown();
+    }
 }
 
 int main(int argc, char** argv)
@@ -96,10 +262,18 @@ int main(int argc, char** argv)
     //namedWindow("output", WINDOW_NORMAL);
     int key = 0;
 
+    int W = 0x11; //key number(i think)
+
+    std::cout << "Window name: ";
+    std::string window_name;
+    std::cin >> window_name;
+
+    FLIGHT_MODES mode = TAKEOFF;
+
     while (key != 27)
     {
         //HWND hwndDesktop = GetWindow(GetCapture(), 1);
-        auto s = FindWindowA(NULL, "Grand Theft Auto V");//Grand Theft Auto V | opencv_filter_test - Paint
+        auto s = FindWindowA(NULL, window_name.c_str());
         HWND hwndDesktop = s;
         //std::cout << hwndDesktop;
 
@@ -110,10 +284,23 @@ int main(int argc, char** argv)
         // you can do some image processing here
         imshow("output", src);
 
-        auto new_screen = process_img(src);
-        imshow("фылтр", new_screen);
+        //if (height > something)
+            //mode = FLY;
 
-        key = waitKey(27); // you can change wait time
+        if (mode != FLY)
+            controlSystem(LAND, mode);
+        
+        for (auto instruction : findDirection(src))
+        {
+            if (instruction == LAND)
+                mode = LANDING;
+            controlSystem(instruction, mode);
+        }
+
+        auto new_screen = process_img(src);
+        imshow("фы", new_screen);
+
+        key = waitKey(60); // you can change wait time
     }
 
 }
